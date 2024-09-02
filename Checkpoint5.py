7fac2fcdd190d7db905607663153443d1317ebcc
Nayris <nayra.mlss@gmail.com>
2024-09-02 20:45:22 +0200

    first commit

---
+++ Checkpoint5.py
@@ -0,0 +1,27 @@
+#Cree un bucle For de Python.
+
+for num in range (0,50,5):
+  print(num)
+
+#Cree una función de Python llamada suma que tome 3 argumentos y devuelva la suma de los 3.
+def suma(a,b,c):
+  return a + b + c
+resultado = suma(13, 12, 18)
+print(resultado)
+
+#Cree una función lambda con la misma funcionalidad que la función de suma que acaba de crear.
+
+suma_lambda = lambda a, b, c: a + b + c
+resultado = suma_lambda(13, 15, 18)
+
+print(resultado)
+
+#-Utilizando la siguiente lista y variable, determine si el valor de la variable coincide o no con un valor de la lista. *Sugerencia, si es necesario, utilice un bucle for in y el operador in.
+
+nombre = 'Enrique'
+lista_nombre =['Jessica', 'Paul', 'George', 'Henry', 'Adán']
+
+if nombre in lista_nombre:
+  print(f"El nombre {nombre} está en la lista.")
+else:
+    print(f"El nombre {nombre} no está en la lista.")
